37c5f11ba75d7b6f0a4b4c0048e6249bf90cdda3
Monakrish <mohanakrishnan02@gmail.com>
2022-03-08 11:27:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const search = document.getElementById("srch");
+const form = document.getElementById("form");
+
+form.addEventListener('submit',function(event){
+
+    event.preventDefault();
+
+    const searchTerm = srch.value;
+
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+searchTerm)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        showMeals(response.meals);
+    });
+});
+
+function showMeals(meals){
+    result.innerHTML="";
+
+    meals.forEach(function(food){
+        const foodCover = food.strMealThumb;
+        const foodName  = food.strMeal;
+       const foodLink = food.strYoutube;
+      
+
+        //  const divElemnt = document.createElement("div");
+        const anchorElement = document.createElement("a");
+        const imageElement = document.createElement("img");
+         const titleElement = document.createElement('h2')
+         
+       
+      
+        anchorElement.href=foodLink;
+        anchorElement.target="";
+        imageElement.src = foodCover;
+        imageElement.title = foodName;
+         titleElement.innerText=foodName;
+      
+          anchorElement.append(imageElement);
+       
+        //  divElemnt.append(imageElement);
+        //  divElemnt.append(anchorElement);
+         anchorElement.append(titleElement);
+        // divElemnt.append(titleElement);
+       
+         anchorElement.className ='meal-card';
+        //  divElemnt.className ='meal-card';
+        //  result.append(divElemnt);
+         result.append(anchorElement);
+    });
+    
+
+}
+
